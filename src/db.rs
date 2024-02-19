c23c8a8717110b370365810ffd03525e9cf01096
Jarek Rudzinski <jarek@chunski.com>
2024-02-19 13:10:18 -0800

    initial commit

---
+++ src/db.rs
@@ -0,0 +1,141 @@
+use const_format::concatcp;
+use rusqlite::{named_params, Connection, Error, OpenFlags};
+use std::path::Path;
+
+const URL_MAPPINGS_TABLE: &str = "url_mappings";
+const URL_MAPPINGS_DDSQL: &str = concatcp!(
+    "CREATE TABLE IF NOT EXISTS ",
+    URL_MAPPINGS_TABLE,
+    " (
+        id INTEGER PRIMARY KEY,
+        long_url TEXT,
+        url_hash INTEGER)"
+);
+
+const GET_BY_ID_MAPPINGS_SQL: &str = concatcp!(
+    "SELECT * FROM ",
+    URL_MAPPINGS_TABLE,
+    " where  id = :id"
+);
+
+const GET_BY_URL_HASH_SQL: &str = concatcp!(
+    "SELECT * FROM ",
+    URL_MAPPINGS_TABLE,
+    " where  url_hash = :url_hash"
+);
+
+const GET_BY_LONG_URL_SQL: &str = concatcp!(
+    "SELECT * FROM ",
+    URL_MAPPINGS_TABLE,
+    " where  long_url = :long_url"
+);
+
+const INSERT_INTO_MAPPINGS_SQL: &str = concatcp!(
+    "INSERT INTO ",
+    URL_MAPPINGS_TABLE,
+    "( long_url, url_hash )
+     VALUES ( :long_url, :url_hash )"
+);
+
+pub struct Db<'a> {
+    path: &'a Path,
+}
+
+impl Db<'_> {
+    pub fn new(dbpath: &Path) -> Db {
+        Db { path: dbpath }
+    }
+
+    fn connect(&self) -> Connection {
+        match Connection::open_with_flags(
+            self.path,
+            OpenFlags::SQLITE_OPEN_READ_WRITE
+            | OpenFlags::SQLITE_OPEN_CREATE
+            | OpenFlags::SQLITE_OPEN_URI
+            | OpenFlags::SQLITE_OPEN_NO_MUTEX,
+        ) {
+            Ok(conn) => conn,
+            Err(why) => panic!("{}", why),
+        }
+    }
+
+    pub fn init_schema(&self) -> () {
+        let conn = self.connect();
+        for sql in [
+            URL_MAPPINGS_DDSQL
+        ] {
+            self.create_schema(&conn, sql);
+        }
+    }
+
+    fn create_schema(&self, conn: &Connection, sql: &str) -> usize {
+        match conn.execute(sql, ()) {
+            Ok(res) => res,
+            Err(why) => panic!("schema create failed: {}", why),
+        }
+    }
+}
+
+pub struct UrlMapping {
+    pub id: i64,
+    pub long_url: String,
+    pub url_hash: i64,
+}
+
+impl UrlMapping {
+    pub fn new(id: i64, long_url: String, url_hash: i64) -> UrlMapping {
+        UrlMapping {
+            id, long_url, url_hash,
+        }
+    }
+
+    pub fn get_slug(&self) -> String {
+        format!("{:x}", self.url_hash)
+    }
+
+    pub fn insert(db: &Db, mapping: &UrlMapping) -> Result<i64, Error> {
+        let conn = db.connect();
+        let mut stmt = conn.prepare(INSERT_INTO_MAPPINGS_SQL).expect("prepare failed");
+        stmt.insert(named_params! {":long_url": mapping.long_url, ":url_hash": mapping.url_hash })
+    }
+
+    pub fn query_by_url_hash(db: &Db, url_hash: i64) -> Option<UrlMapping> {
+        let conn = db.connect();
+        let mut stmt = conn.prepare(GET_BY_URL_HASH_SQL).expect("prepare failed");
+        let mut row_iter = stmt.query(named_params! {":url_hash": url_hash }).unwrap();
+
+        match row_iter.next().expect("next failed") {
+            Some(row) => Some(UrlMapping {
+                id: row.get(0).unwrap(),
+                long_url: row.get(1).unwrap(),
+                url_hash: row.get(2).unwrap(),
+            }),
+            None => None,
+        }
+    }
+
+    pub fn query_by_long_url(db: &Db, long_url: String) -> Result<Vec<UrlMapping>, Error>{
+        let conn = db.connect();
+        let mut stmt = conn.prepare(GET_BY_LONG_URL_SQL).expect("prepare failed");
+
+        stmt.query_map([], |r| {
+            Ok(UrlMapping::new(r.get(0)?, r.get(1)?, r.get(2)?))
+        }).unwrap().collect()
+    }
+
+
+    pub fn query_by_id(db: &Db, id: u32) -> Option<UrlMapping> {
+        let conn = db.connect();
+        let mut stmt = conn.prepare(GET_BY_ID_MAPPINGS_SQL).expect("prepare failed");
+        let mut row_iter = stmt.query(named_params! {":id": id.to_string()}).unwrap();
+
+        match row_iter.next().expect("next failed") {
+            Some(row) => Some(UrlMapping {
+                id: row.get(0).unwrap(),
+                long_url: row.get(1).unwrap(),
+                url_hash: row.get(2).unwrap(),
+            }),
+            None => None,
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,80 @@
+use axum::http::{StatusCode, Uri};
+use axum::{ routing, Router,};
+use rusqlite::{named_params, Connection, Error, OpenFlags};
+
+use axum::extract::{Path, Query, Json, State};
+use std::collections::HashMap;
+use std::hash::{DefaultHasher, Hash, Hasher};
+use std::sync::Arc;
+
+use serde::Deserialize;
+
+mod db;
+use db::{ Db, UrlMapping };
+
+
+#[derive(Deserialize)]
+struct ShortUrlRequest {
+    longUrl: String,
+}
+
+
+struct AppState<'a> {
+    db: Db<'a>
+}
+
+struct AppError {
+
+}
+
+async fn get_shorten_url(
+    State(state): State<Arc<AppState<'_>>>, url: String) -> Result<String, StatusCode> {
+
+    let mut hasher = DefaultHasher::new();
+    for khar in url.as_bytes() {
+        hasher.write_u8(*khar);
+    }
+    let shortenedUrl = hasher.finish();
+
+    let mapping = UrlMapping::new(0, url.clone(), shortenedUrl as i64);
+
+    println!("got shorten request for {}", url);
+
+    match UrlMapping::insert(&state.db, &mapping) {
+        Ok(_) => (),
+        Err(e) => { println!("Err: {}", e); return Err(StatusCode::INTERNAL_SERVER_ERROR); }
+    }
+
+    Ok(format!("https://localhost:8000/expand/{}", mapping.get_slug()))
+}
+
+async fn get_expanded_url(
+    State(state): State<Arc<AppState<'_>>>, Path(slug): Path<String>) -> Result<String, StatusCode> {
+
+    let url_hash = match i64::from_str_radix(&slug, 16) {
+        Ok(foo) => foo,
+        Err(_) => return Err(StatusCode::BAD_REQUEST),
+    };
+    println!("Got request for {}", url_hash);
+    let result = UrlMapping::query_by_url_hash(&state.db, url_hash);
+    match result {
+        Some(mapping) => Ok(mapping.long_url.to_owned()),
+        None => Err(StatusCode::NOT_FOUND)
+    }
+}
+
+#[tokio::main]
+async fn main() {
+
+    let dbpath = std::path::Path::new("mappings.db");
+    let shared_state = Arc::new(AppState { db: Db::new(dbpath) });
+    shared_state.db.init_schema();
+
+    let app = Router::new()
+        .route("/", routing::get(|| async { "Hello, World!\n" }))
+        .route("/shorten", routing::post(get_shorten_url)).with_state(shared_state.clone())
+        .route("/expand/:slug", routing::get(get_expanded_url)).with_state(shared_state.clone());
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
