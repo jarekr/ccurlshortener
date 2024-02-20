471d1b655a194181380ab5570087aa34f11f949d
Jarek Rudzinski <jarek@chunski.com>
2024-02-20 12:52:58 -0800

    file: styles/tailwind.css file: tailwind.config.js .gitignore

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./templates/*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
