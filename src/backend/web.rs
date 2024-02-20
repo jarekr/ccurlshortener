8acfb5066ee79e613e9d26bc038a7b9cf2fe5c27
Jarek Rudzinski <jarek@chunski.com>
2024-02-19 22:40:29 -0800

    file: Cargo.lock file: src/backend.rs file: src/backend/web.rs

---
+++ src/backend/web.rs
@@ -0,0 +1,3 @@
+use const_format::concatcp;
+pub const HEADER_TEMPLATE: &str = concatcp!("<a href=\"/\">home</a> |");
+pub const FOOTER_TEMPLATE: &str = concatcp!("<div class=\"footer\">copyright 2024 chunski industries</div>");
